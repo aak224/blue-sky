9dc844769b0d96b623b375d87e4d70fb73c872cc
aak224 <160564752+aak224@users.noreply.github.com>
2024-02-19 19:57:21 -0800

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,11 @@
+print('Input a number', end='')
+number1 = int(input())
+
+print('Input a second number', end='')
+number2 = int(input())
+
+Added = number1 + number2 
+print(Added)
+
+Subtracted = number1 - number2 
+print(Subtracted)
---
+++ part2.py
@@ -0,0 +1,12 @@
+print('Input a number', end='')
+number1 = int(input())
+
+print('Input a second number', end='')
+number2 = int(input())
+ 
+Multiplied = number1 * number2
+print(Multiplied)
+
+Divided = number1 / number2 
+print(Divided)
+
